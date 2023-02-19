0a1a1fec9e5acfa083fc0f4d40cbbfb035daa7dd
Zachary King <zking@Saiph.local>
2023-02-19 10:28:51 -0600

    solved d1p1. Elf withmost has 11377972 calories

--- day_1/p1.py
+++ day_1/p1.py
@@ -1,21 +1,11 @@
-from itertools import groupby
+cals = 0
+elf_cals_total = []
+with open("C3/day_1/p1_input.txt", 'r') as f:
+    for line in f:
+        if line != "\n":
+            cals = cals + int(line)
 
-with open("C3/day_1/p1_input.txt", 'r') as elf_cals:
-    elf_cals_list = [i.split('\n', 1)[0] for i in elf_cals.readlines()]  #reads the file and removes the newline characters
+        else:
+            elf_cals_total.append(cals)
 
-
-
-
-
-###   Solutions I've Tried that didn't work as desired   ###
-
-#elf_cals_index = elf_cals_list.index('')
-#elf_cals_split = [elf_cals_list.split(i, 1)[0] for i in elf_cals_index]
-
-##I honestly dont know how this works, I found this solution for spliting the list to a list of lists here:
-##https://www.geeksforgeeks.org/python-splitting-list-on-empty-string/
-#elf_cals_split = [list(sub) for ele, sub in groupby(elf_cals_list, key = bool) if ele]
-
-
-
-print (elf_cals_list)
+print(max(elf_cals_total))
