d306d5dfc83f96a5b9ed79ed42a4e5f15ab9bda1
Zachary King <zking@Saiph.local>
2023-02-19 11:06:19 -0600

    added d1 p2 code

--- day_1/p1.py
+++ day_1/p1.py
@@ -7,5 +7,10 @@ with open("C3/day_1/p1_input.txt", 'r') as f:
 
         else:
             elf_cals_total.append(cals)
+            cals = 0
 
 print(max(elf_cals_total))
+
+ect_sorted = sorted(elf_cals_total, reverse=True)
+
+print(ect_sorted[0] + ect_sorted[1] + ect_sorted[2])
